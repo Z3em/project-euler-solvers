e531623ef710d129ac45c028ec58827c5910c198
Anem <anem@protonmail.com>
2020-03-01 01:51:01 +0500

    Added problem #6

---
+++ euler_0006/sum_square_difference.py
@@ -0,0 +1,9 @@
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+sumOfSquares = 0
+for i in range(1, 101):
+    sumOfSquares += i**2
+
+squareOfSum = (101*50)**2
+
+print(squareOfSum-sumOfSquares)
